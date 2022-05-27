81e8a28eb51c6b417b016ac603aacb7454d5564c
whitewAw <whitewAw@gmail.com>
2022-05-27 10:04:03 +0300

    add a LiquidityIndicatorsOfBalance tabl (#23)
    Co-authored-by: Oleksandr Shevchenko4 <oleksandr_shevchenko4@epam.com>

---
+++ AFS.Core/Models/TablsModels/LiquidityIndicatorsOfBalanceRow.cs
@@ -0,0 +1,12 @@
+﻿namespace AFS.Core.Models.TablsModels
+{
+    public class LiquidityIndicatorsOfBalanceRow
+    {
+        public double ABegin { get; set; }
+        public double AEnd { get; set; }
+        public double PBegin { get; set; }
+        public double PEnd { get; set; }
+        public double PaymentBalanceBegin => ABegin - PBegin;
+        public double PaymentBalanceEnd => AEnd - PEnd;
+    }
+}
