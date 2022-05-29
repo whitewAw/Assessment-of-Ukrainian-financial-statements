5a9d7d3c85d36060be3da400cda89f1fda1494b4
whitewAw <whitewAw@gmail.com>
2022-05-29 18:12:44 +0300

    Refactoring (#33)
    * add meta data
    
    * add a default file
    
    Co-authored-by: Oleksandr Shevchenko4 <oleksandr_shevchenko4@epam.com>

---
+++ AFS.Core/Services/DataCalculations/WorkingCapitalTurnoverTime.cs
@@ -0,0 +1,63 @@
+﻿using AFS.Core.Model;
+using AFS.Core.Models;
+
+namespace AFS.Core.Services.DataCalculations
+{
+    public class WorkingCapitalTurnoverTimeChart
+    {
+        FactorsAffectingTurnoverOfWorkingCapital FactorsAffectingTurnoverOfWorkingCapital { get; set; }
+        AFSModel model { get; set; }
+
+        public WorkingCapitalTurnoverTimeChart(AFSModel model) => Init(model);
+        private void Init(AFSModel model)
+        {
+            FactorsAffectingTurnoverOfWorkingCapital = new(model);
+            this.model = model;
+        }
+        public List<ChartDateTimeItem> GetMoney()
+        {
+            List<ChartDateTimeItem> items = new();
+            items.Add(new ChartDateTimeItem
+            {
+                Date = new DateTime(model.BaseYear, 1, 1),
+                Value = FactorsAffectingTurnoverOfWorkingCapital.Money.BaseYear
+            });
+            items.Add(new ChartDateTimeItem
+            {
+                Date = new DateTime(model.CurrentYear, 12, 31),
+                Value = FactorsAffectingTurnoverOfWorkingCapital.Money.CurrentYear
+            });
+            return items;
+        }
+        public List<ChartDateTimeItem> GetReceivables()
+        {
+            List<ChartDateTimeItem> items = new();
+            items.Add(new ChartDateTimeItem
+            {
+                Date = new DateTime(model.BaseYear, 1, 1),
+                Value = FactorsAffectingTurnoverOfWorkingCapital.Receivables.BaseYear
+            });
+            items.Add(new ChartDateTimeItem
+            {
+                Date = new DateTime(model.CurrentYear, 12, 31),
+                Value = FactorsAffectingTurnoverOfWorkingCapital.Receivables.CurrentYear
+            });
+            return items;
+        }
+        public List<ChartDateTimeItem> GetMaterialValues()
+        {
+            List<ChartDateTimeItem> items = new();
+            items.Add(new ChartDateTimeItem
+            {
+                Date = new DateTime(model.BaseYear, 1, 1),
+                Value = FactorsAffectingTurnoverOfWorkingCapital.MaterialValues.BaseYear
+            });
+            items.Add(new ChartDateTimeItem
+            {
+                Date = new DateTime(model.CurrentYear, 12, 31),
+                Value = FactorsAffectingTurnoverOfWorkingCapital.MaterialValues.CurrentYear
+            });
+            return items;
+        }
+    }
+}
